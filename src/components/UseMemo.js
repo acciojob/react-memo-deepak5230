3cb529f954035b25a9f4f6d8736562e8384bf9aa
deepak <deepakgupta351@gmail.com>
2024-08-02 15:48:54 +0530

    finished

---
+++ src/components/UseMemo.js
@@ -0,0 +1,31 @@
+
+import React, { useMemo, useState } from 'react';
+
+function UseMemo({ todos }) {
+      const [count, setCount] = useState(0);
+      const dynamicContent = useMemo(() => {
+        return 1000000000 + count;
+      }, [count]);
+  const handleIncrement = () => {
+    setCount(count + 1);
+    console.log(`count after increment ${count}`);
+  };
+  return (
+    <div id="calc">
+        <p id="incr-cnt">Count: {count}</p>
+      <p>{dynamicContent}</p>
+      <button id="incr-btn" onClick={handleIncrement}>
+        + {count}
+      </button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li id={`todo-${index}`} key={index}>{todo}
+          
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UseMemo;
